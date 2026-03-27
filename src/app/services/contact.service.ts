4c018faea2a1ab1407003b13b16fde782d4b43b1
islinas1 <iesalinas@umsa.bo>
2026-03-27 13:18:16 -0400

    corregir rutas del auth service

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,15 +1,15 @@
+import { environment } from '../../environments/environment';
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Contact } from '../interfaces/contact.interface';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContactService {
   private http = inject(HttpClient);
-  private readonly API_URL = environment.apiUrl;
+  private readonly API_URL = `${environment.apiUrl}/contacts`;
 
   getContacts(search: string = ''): Observable<Contact[]> {
     let params = new HttpParams();
